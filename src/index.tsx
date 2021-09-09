75c774a7ff8165de28eee2c4bf55df6bdbae0765
kwhong95 <kwhong95@kakao.com>
2021-09-09 17:58:19 +0900

    [DEV]First Commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App: React.FC = () => {
+  const [count, setCount] = useState<number>(0);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(() => {
+    setTimeout(() => {
+      if(count === 20) {
+        clearTimeout();
+      } else {
+        setCount(count + 1);
+      }
+    }, 1000);
+
+  }, [count]);
+   
+  return (
+    <h1>{count}</h1>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
