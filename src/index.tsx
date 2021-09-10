3aa8a6f1c6f32fc68e7adf563aad8c63e3914915
kwhong95 <kwhong95@kakao.com>
2021-09-10 17:23:38 +0900

    [DEV]Create Screen Saver

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,85 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import ReactDOM from 'react-dom';
 
-const App: React.FC = () => {
-  const [count, setCount] = useState<number>(0);
+const SCREENSAVER_DELAY_MS = 3000;
+const SCREENSAVER_ACTIVE_TIME_MS = 7000;
+const SCREENSAVER_INACTIVE_TIME_MS = 5000;
+
+const App = () => {
+  const [screensaverActive, setScreensaverActive] = useState<boolean>(true);
+  const [screensaverVisible, setScreensaverVisible] = useState<boolean>(true);
+  const screensaverTimeout = useRef<any>();
+  const secondTimer = useRef<any>();
+
+  const activeScreensaver = useCallback(() => {
+    setScreensaverActive(true);
+    setScreensaverVisible(true);
+    loop();
+
+    function loop() { 
+      const timeRef = setTimeout(() => {
+        setScreensaverActive(false);
+        const timeRef2 = setTimeout(() => {
+          setScreensaverVisible(false);
+          loop();
+        }, SCREENSAVER_INACTIVE_TIME_MS);
+        secondTimer.current = timeRef2;
+      }, SCREENSAVER_ACTIVE_TIME_MS);
+      secondTimer.current = timeRef;
+    }
+  }, []);
 
   useEffect(() => {
-    setTimeout(() => {
-      if(count === 20) {
-        clearTimeout();
-      } else {
-        setCount(count + 1);
-      }
-    }, 1000);
+    activeScreensaver();
+  }, [ activeScreensaver ]);
 
-  }, [count]);
-   
+  const screensaverClicked = useCallback(() => {
+    setScreensaverActive(false);
+    startTimeout();
+  }, []);
+
+  const startTimeout = useCallback(() => {
+    clearTimeout(screensaverTimeout.current);
+    clearTimeout(secondTimer.current);
+    const timeout = setTimeout(() => activeScreensaver(), SCREENSAVER_DELAY_MS);
+    screensaverTimeout.current = timeout;
+  }, []);
+
+  const appTouched = useCallback((ev) => {
+    if (ev.target.id !== "screensaver") {
+      startTimeout();
+    }
+  }, []);
+
   return (
-    <h1>{count}</h1>
+    <main onClick={appTouched}>
+      {screensaverActive && screensaverVisible && (
+        <div
+          id="screensaver"
+          style={styles.screensaver as any}
+          onClick={screensaverClicked}
+        >
+          SCREENSAVER
+        </div>
+      )}
+    </main>
   );
 }
+
+const styles = {
+  screensaver: {
+    position: "fixed",
+    left: 0,
+    top: 0,
+    width: "100%",
+    height: "100%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(100, 100, 150, 0.92)",
+    fontSize: 32,
+    color: "white"
+  }
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
---
+++ src/screen/screen-saver.tsx
@@ -0,0 +1,5 @@
+const ScreenSaver = () => {
+  return <h1>Screen Saver</h1>
+}
+
+export default ScreenSaver;
