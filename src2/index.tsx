1f206fa5a89c04b15718da619d472a71c10438b8
kwhong95 <kwhong95@kakao.com>
2021-11-20 19:22:11 +0900

    Delete duplicate project files

--- src2/index.tsx
+++
@@ -1,5 +0,0 @@
-import { render } from "react-dom";
-
-import App from "./layouts/App";
-
-render(<App />, document.getElementById("root"));
--- src2/layouts/Container.tsx
+++
@@ -1,9 +0,0 @@
-interface Props {
-  current: string;
-}
-
-const Container: React.FC<Props> = ({ children }) => {
-  return <div>{children}</div>;
-};
-
-export default Container;
